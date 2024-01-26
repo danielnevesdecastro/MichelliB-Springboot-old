012f0b3c71afa3aa58341fbf33277bb680548d36
daniel.castro <daniel.castro@unimedjp.com.br>
2024-01-26 11:40:53 -0300

    feat:Listar eventos

--- src/main/java/br/com/evento/controller/EventoController.java
+++ src/main/java/br/com/evento/controller/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.evento.model.Evento;
 import br.com.evento.repository.EventoRepository;
@@ -32,5 +33,12 @@ public class EventoController {
 		return "redirect:/cadastrarEvento";
 	}
 	
+	@RequestMapping(value = "/eventos" , method =RequestMethod.GET)
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
 	
 }
